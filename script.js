0c4a78b6e24cd0a899ddec338068875e6919cfd5
Reena Thomas <rtthomas@ucsc.edu>
2023-07-06 15:41:32 -0700

    song

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const audio = document.getElementById('my-audio');
-  const playButton = document.getElementById('play-button');
-  const pauseButton = document.getElementById('pause-button');
-
-  playButton.addEventListener('click', () => {
-    audio.play();
-  });
-
-  pauseButton.addEventListener('click', () => {
-    audio.pause();
-  });
+function playAudio() { const audio = document.getElementById('audio'); audio.play(); } function pauseAudio() { const audio = document.getElementById('audio'); audio.pause(); }
